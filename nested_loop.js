ba70684273c4e1d50b4efe1bba144228559b1dc2
Nayem1004 <nayem@procoder.org>
2025-04-10 15:35:47 +0600

    nested loop practice

---
+++ nested_loop.js
@@ -0,0 +1,12 @@
+var n = 10;
+
+for (var i = 1; i <= n; i++) {
+    var result = '';
+    for (var j = 1; j <= n - i; j++) {
+        result += ' ';
+    }
+    for (var k = 1; k <= i; k++) {
+        result += '*';
+    }
+    console.log(result);
+}
